7973dd2db3d9afbd87610d459b1dd390d3c471de
Nicole Dow <nikki.dow71@gmail.com>
2019-07-25 14:30:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,10 @@ class Anagram
   end
   
   def match(array)
-    
+    #iterate over the array of words
+      #compare the letters in each word of the array to @word
+      #if they match, shovel into an array of matches
+      #return the array of matches
   end
   
 end
