ba7e090faab49b5af91299384e3cbf0f9b35694c
Nicole Dow <nikki.dow71@gmail.com>
2019-07-25 14:37:37 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+
+
+class Anagram
+  attr_reader :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    anagrams = []
+    #iterate over the array of words
+    array.each do |possible_match|
+      #compare the letters in each word of the array to @word
+      if possible_match.split("").sort == word.split("").sort 
+      #if they match, shovel into an array of matches
+        anagrams << possible_match
+      end
+    end
+    #return the array of matches
+  anagrams
+  end
+
+end
