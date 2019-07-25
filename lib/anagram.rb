b85db4328a4f89e109acb81ded96ea2c1e759740
Nicole Dow <nikki.dow71@gmail.com>
2019-07-25 14:35:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,7 +12,7 @@ class Anagram
     #iterate over the array of words
     array.each do |possible_match|
       #compare the letters in each word of the array to @word
-      if possible_match.split(" ").sort == word.split("").sort 
+      if possible_match.split("").sort == word.split("").sort 
       #if they match, shovel into an array of matches
         anagrams << possible_match
         binding.pry
